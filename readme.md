4df2273e5717879c62bc598f0a38848a226a8a36
Bui Thanh Hai <haibt@anymindgroup.com>
2022-05-30 19:10:40 +0700

    initial project

---
+++ readme.md
@@ -0,0 +1,1 @@
+Article: https://medium.com/p/1aaf3d3479ea/edit
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const dependencies = require("./package.json").dependencies;
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  devtool: "inline-source-map",
+  mode: isDevelopment ? "development" : "production",
+  entry: [path.resolve("src/index.jsx")],
+  output: {
+    path: path.resolve("build"),
+    filename: "[name].js",
+    chunkFilename: "[name].js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  devServer: {
+    open: false,
+    hot: true,
+    historyApiFallback: true,
+    port: 3000,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "Test",
+      library: { type: "var", name: "Test" },
+      filename: "test.js",
+      exposes: {
+        "./App": "./src/App",
+      },
+      shared: {
+        react: {
+          eager: true,
+          singleton: true,
+          requiredVersion: dependencies.react,
+        },
+        "react-dom": {
+          eager: true,
+          singleton: true,
+          requiredVersion: dependencies["react-dom"],
+        },
+      },
+    }),
+    new HTMLWebpackPlugin({
+      template: path.resolve("public/index.html"),
+      filename: "index.html",
+      chunksSortMode: "none",
+    }),
+  ],
+};
